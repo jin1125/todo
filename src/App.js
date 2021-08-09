f8d39bacbfc25d800fdf8cc1ff3e8e5143592400
jin1125 <shogo.jinta@gmail.com>
2021-08-09 21:25:48 +0900

    starge1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export const App = () => {
+  const [todos, setTodos] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+  const [incomplete, setIncomplete] = useState([
+    {
+      id: 1,
+      todos: "勉強する",
+      deadline: "2021-08-10",
+      status: "未完了",
+    },
+  ]);
+
+  const id = incomplete.length + 1;
+
+  const data = {
+    id: id,
+    todos: todos,
+    deadline: deadline,
+    status: "未完了",
+  };
+
+  const inputTodos = (e) => {
+    setTodos(e.target.value);
+  };
+
+  const inputDeadline = (e) => {
+    setDeadline(e.target.value);
+  };
+
+  const inputButton = () => {
+    setIncomplete([...incomplete, data]);
+    setTodos("");
+    setDeadline(null);
+  };
+
+  const allDeleteButton = () => {
+    const result = window.confirm("全てのタスクを消しますか？");
+    if (result) {
+      setIncomplete([]);
+    }
+  };
+
+  const deleteButton = (index) => {
+    const result = window.confirm(
+      `ID : ${index+1} のタスクを消しますか？`
+    );
+    if (result) {
+      setIncomplete(
+        incomplete.filter((todo)=>(
+          todo.id !== index+1
+        ))
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <label htmlFor="todo">タスク入力</label>
+      <input
+        type="text"
+        id="todo"
+        name="todo"
+        value={todos}
+        onChange={inputTodos}
+      />
 
-export default App;
+      <label htmlFor="deadline">期日</label>
+      <input type="date" id="date" name="deadline" onChange={inputDeadline} />
+
+      <button onClick={inputButton}>タスクを追加</button>
+      <button onClick={allDeleteButton}>全てのタスクを削除</button>
+
+      {incomplete.map((todo, index) => (
+        <div key={index}>
+          <ul>
+            <li>{`ID : ${index+1}`}</li>
+            <li>{`タスク : ${todo.todos}`}</li>
+            <li>{`期日 : ${todo.deadline}`}</li>
+            <li>{`ステータス : ${todo.status}`}</li>
+          </ul>
+          <button onClick={()=>deleteButton(index)}>
+            削除
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
